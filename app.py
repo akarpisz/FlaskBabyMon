5b3cb9fd638ff67c2f7fd393c10c94b0eee781b4
andrew karpisz <andrew.karpisz@gmail.com>
2020-11-03 11:27:10 -0700

    linked css file and javascript file. cleaned up backend syntax

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify, send_file
 import os 
+
+dirname = os.path.dirname(__file__)
+home = os.path.join(dirname, 'static', 'index.html')
+
 app = Flask(__name__)
 
 
@@ -9,5 +13,9 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 
 def index():
-    return send_file("index.html")
+    # return send_file("index.html")
+    return send_file(home)
+
+if __name__ == "__main__":
+    app.run()
     
